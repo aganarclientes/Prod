938cb2162c9d2b99a569aa2cd9c90715b9321ffd
agc2020 <aganarclientes@gmail.com>
2021-02-19 00:52:16 -0300

    formulario php

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+    $destino= "info@aganarclientes.com.ar";
+    $nombre= $_POST["Nombre"];
+    $apellido= $_POST["Apellido"];
+    $email= $_POST["Email"];
+    $producto= $_POST["Producto"];
+    $mensaje= $_POST["Mensaje"];
+    $contenido = "Nombre: " . $nombre ."\nApellido: " . $apellido ."\nEmail: " . $email ."\nProducto: " . $producto ."\nMensaje: " . $mensaje;
+    mail($destino,"Formulario de Contacto", $Contenido);
+    header("Location:agradecimiento_contacto.html");
+?>
