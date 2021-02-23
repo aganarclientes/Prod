29d98ce0afde3a22e8ce64f243ca3c6115acc131
agc2020 <aganarclientes@gmail.com>
2021-02-23 18:34:50 -0300

    nuevo captcha sin php

--- enviar.php
+++ enviar.php
@@ -1,11 +1,11 @@
 <?php
-    $destino= "info@aganarclientes.com.ar";
-    $nombre= $_POST["Nombre"];
-    $apellido= $_POST["Apellido"];
-    $email= $_POST["Email"];
-    $producto= $_POST["Producto"];
-    $mensaje= $_POST["Mensaje"];
+    $destino = "info@aganarclientes.com.ar";
+    $nombre = $_POST["Nombre"];
+    $apellido = $_POST["Apellido"];
+    $email = $_POST["Email"];
+    $producto = $_POST["Producto"];
+    $mensaje = $_POST["Mensaje"];
     $contenido = "Nombre: " . $nombre ."\nApellido: " . $apellido ."\nEmail: " . $email ."\nProducto: " . $producto ."\nMensaje: " . $mensaje;
-    mail($destino,"Formulario de Contacto", $Contenido);
+    mail($destino,"Formulario de Contacto", $contenido);
     header("Location:agradecimiento_contacto.html");
 ?>
