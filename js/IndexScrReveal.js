ce3efad066de644e29a1e22bf884869205449225
agc2020 <aganarclientes@gmail.com>
2021-02-27 12:08:27 -0300

    scrollreveal

---
+++ js/IndexScrReveal.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header-tel', {
+        duration: 1500,
+        origin: 'bottom',
+        distance : '-200px'
+    });
+
+    sr.reveal('.logo', {
+        duration: 3000,
+        origin: 'left',
+        distance : '400px'
+    });
+
+    sr.reveal('#navbar-right, .columns', {
+        duration: 4000,
+        origin: 'right',
+        distance : '400px'
+    });
+
+    sr.reveal('.grafico', {
+        duration: 4000,
+        origin: 'left',
+        distance : '300px'
+    });
+
+    sr.reveal('.hero-text', {
+        duration: 4000
+    });
