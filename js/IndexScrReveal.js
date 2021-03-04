3bcb728fa99ef05a40725e43297764a521dc14a2
agc2020 <aganarclientes@gmail.com>
2021-03-04 00:15:14 -0300

    ultimos detalles de aganarclientes

--- js/IndexScrReveal.js
+++ js/IndexScrReveal.js
@@ -1,29 +1,35 @@
 window.sr = ScrollReveal();
+/*window.sr = new ScrollReveal({ mobile: false });*/
 
     sr.reveal('.header-tel', {
         duration: 1500,
         origin: 'bottom',
-        distance : '-200px'
+        distance : '-200px',
+        mobile: false
     });
 
     sr.reveal('.logo', {
         duration: 3000,
         origin: 'left',
-        distance : '400px'
+        distance : '400px',
+        mobile: false
     });
 
     sr.reveal('#navbar-right, .columns1', {
         duration: 4000,
         origin: 'right',
-        distance : '400px'
+        distance : '400px',
+        mobile: false
     });
 
     sr.reveal('.grafico', {
         duration: 4000,
         origin: 'left',
-        distance : '300px'
+        distance : '300px',
+        mobile: false
     });
 
     sr.reveal('.hero-text , .columns-contacto', {
-        duration: 4000
+        duration: 4000,
+        mobile: false
     });
